94a68fa8e34851eeb41c3009aefaf9940649f928
Alexandre Betioli <alexandre.betioli@beatgridmedia.com>
2025-05-28 22:57:25 +0200

    Refactor

--- particle.js
+++ particle.js
@@ -1,5 +1,9 @@
 class Particle {
-  constructor(position, colors = [[255, 255, 255]], density = 10) {
+  constructor(
+    position,
+    colors = [[255, 255, 255]],
+    density = 10
+  ) {
     const colorIndex = Math.round(Math.random() * (colors.length - 1));
     this.color = colors[colorIndex];
     this.position = position;
@@ -7,52 +11,53 @@ class Particle {
   }
 
   update(state) {
-    
-    // General rule, particle with more density go to the bottom
-    let { x, y } = this.position;
+    const positions = this.getPositions()
+      .map((p) => ({ x: p[0], y: p[1] }))
+      .filter(({ x, y }) => isWithinBounds(state, x, y))
 
-    // Clear old position
-    state[y][x] = null;
+    const foundEmpty = positions.find(({ x, y }) => isEmpty(state, x, y));
+    if (foundEmpty) {
+      this.position = foundEmpty;
 
-    if (isWithinBounds(state, x, y + 1)) {
-      var below = state[y + 1][x]
-      if (below && this.density > below.density) {
-        swap(this, below)
-        state[y][x] = this
-        state[below.position.y][below.position.x] = below
-        return true
+    } else {
+      const foundLessDensity = positions
+        .map(({ x, y }) => state[y][x])
+        .find((other) => other.density < this.density)
+
+      if (foundLessDensity) {
+        swap(this, foundLessDensity)
+        state[foundLessDensity.position.y][foundLessDensity.position.x] = foundLessDensity
       }
     }
+  }
+
+  getPositions() {
+    const { x, y } = this.position;
+    return [
+      [x, y + 1]
+    ]
   }
 }
 
 export class Sand extends Particle {
   constructor(position) {
-    super(position, [
-      [255, 255, 0], // #FFFF00
-      [240, 230, 140], //#F0E68C
-      [189, 183, 107], // #BDB76B
-    ]);
+    super(
+      position,
+      [
+        [255, 255, 0], // #FFFF00
+        [240, 230, 140], //#F0E68C
+        [189, 183, 107], // #BDB76B
+      ],
+    );
   }
 
-  update(state) {
-    if (super.update(state)) return;
-
-    let { x, y } = this.position;
-
-    const positions = [
+  getPositions() {
+    const { x, y } = this.position;
+    return [
       [x, y + 1],
       [x - 1, y + 1],
       [x + 1, y + 1],
     ];
-
-    const found = positions.find((p) => isEmpty(state, p[0], p[1]));
-    if (found) {
-      [x, y] = found;
-    }
-
-    this.position = { x, y };
-    state[y][x] = this
   }
 }
 
@@ -70,46 +75,25 @@ export class Water extends Particle {
     this.direction = Math.random() > 0.5 ? -1 : 1;
   }
 
-  update(state) {
-    if (super.update(state)) return;
-
-    let { x, y } = this.position;
-
-    const positions = [
+  getPositions() {
+    const { x, y } = this.position;
+    return [
       [x, y + 1],
       [x + this.direction, y],
       [x - this.direction, y],
     ];
-
-    const found = positions.find((p) => isEmpty(state, p[0], p[1]));
-    if (found) {
-      [x, y] = found;
-    }
-
-    this.position = { x, y };
-    state[y][x] = this
   }
 }
 
 export class Rock extends Particle {
   constructor(position) {
-    super(position, [
-      [128, 128, 128], //#808080
-      [211, 211, 211], //#D3D3D3
-    ]);
-  }
-
-  update(state) {
-    if (super.update(state)) return;
-
-    let { x, y } = this.position;
-
-    if (isEmpty(state, x, y + 1)) {
-      y++;
-    }
-
-    this.position = { x, y };
-    state[y][x] = this
+    super(
+      position,
+      [
+        [128, 128, 128], //#808080
+        [211, 211, 211], //#D3D3D3
+      ]
+    );
   }
 }
 
@@ -118,7 +102,7 @@ function isWithinBounds(state, x, y) {
 }
 
 function isEmpty(state, x, y) {
-  return isWithinBounds(state, x, y) && !state[y][x];
+  return !state[y][x];
 }
 
 function swap(a, b) {
